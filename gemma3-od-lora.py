defd123c588499bf86d94c289763e25ede7c1fb8
ariG23498 <aritra.born2fly@gmail.com>
2025-04-22 13:31:59 +0200

    add lora

--- gemma3-od-lora.py
+++ gemma3-od-lora.py
@@ -26,7 +26,8 @@ print(f"{len(train_dataset)=}")
 print(f"{len(val_dataset)=}")
 print(f"{len(test_dataset)=}")
 
-os.makedirs("outputs", exist_ok=True)
+dir_name = "outputs_lora"
+os.makedirs(dir_name, exist_ok=True)
 
 data_sample = train_dataset[-100] # choose any other index to play around
 visualize_bounding_boxes(
@@ -34,7 +35,7 @@ visualize_bounding_boxes(
     data_sample["label_for_paligemma"],
     data_sample["width"],
     data_sample["height"],
-    "outputs/sample.png"
+    f"{dir_name}/sample.png"
 )
 
 processor = AutoProcessor.from_pretrained(cfg.model_id)
@@ -52,9 +53,6 @@ processor = AutoProcessor.from_pretrained(cfg.model_id)
 # g3_tok.add_tokens(location_tokens)
 # processor.tokenizer = g3_tok
 
-# memory pinning for data
-# group offload
-# transformer.enable_group_offload(onload_device=onload_device, offload_device=offload_device, offload_type="leaf_level", use_stream=True)
 
 train_collate_fn = partial(train_collate_fn, processor=processor, dtype=cfg.dtype)
 test_collate_fn = partial(test_collate_fn, processor=processor, dtype=cfg.dtype)
@@ -65,21 +63,21 @@ test_dataloader = DataLoader(test_dataset, batch_size=cfg.batch_size, collate_fn
 model = Gemma3ForConditionalGeneration.from_pretrained(
     cfg.model_id,
     torch_dtype=cfg.dtype,
-    device_map=cfg.device,
+    # device_map=cfg.device,
     # attn_implementation="eager", # As Sergio points out
 )
+model.requires_grad_(False)
 
-# get lora config
-# you always start with a configuration
 lora_config = LoraConfig(
     inference_mode=False,
-    r=1,
-    lora_alpha=4,
+    r=8,
+    lora_alpha=32,
     lora_dropout=0.1,
-    target_modules=["linear"],
+    target_modules=["q_proj", "v_proj",],
     peft_type=PeftType.LORA,
 )
-lora_model = get_peft_model(model=model, peft_config=lora_config)
+lora_model = get_peft_model(model=model, peft_config=lora_config).to(cfg.device)
+lora_model.print_trainable_parameters()
 
 # model.resize_token_embeddings(len(processor.tokenizer)) # to integrate the new tokenizer
 
@@ -88,26 +86,22 @@ sample = sample.to(cfg.device)
 for key, value in sample.items():
     print(key, value.dtype, value.device)
 
-model.eval()
-generation = model.generate(**sample, max_new_tokens=100)
-decoded = processor.batch_decode(generation, skip_special_tokens=True)
+# model.eval()
+# generation = model.generate(**sample, max_new_tokens=100)
+# decoded = processor.batch_decode(generation, skip_special_tokens=True)
 
-for s in decoded:
-    print(s)
+# for s in decoded:
+#     print(s)
 
 
-optimizer = torch.optim.AdamW(model.parameters(), lr=cfg.learning_rate)
-model.train()
+params_to_train = list(filter(lambda x: x.requires_grad, lora_model.parameters())) # Note!
+optimizer = torch.optim.AdamW(params_to_train, lr=cfg.learning_rate)
 
-for name, param in model.named_parameters():
-    if "attn" in name:
-        param.requires_grad = True
-    else:
-        param.requires_grad = False
+# Is the model activations deleted?
 
 for epoch in range(1):
     for idx, batch in enumerate(train_dataloader):
-        outputs = model(**batch.to(cfg.device))
+        outputs = lora_model(**batch.to(cfg.device))
         loss = outputs.loss
         if idx % 100 == 0:
             print(f"Epoch: {epoch} Iter: {idx} Loss: {loss.item():.4f}")
@@ -117,16 +111,17 @@ for epoch in range(1):
         optimizer.zero_grad()
 
 # Push to Hub
-model.push_to_hub("ariG23498/g3-od")
-processor.push_to_hub("ariG23498/g3-od")
+merged_model = lora_model.merge_and_unload()
+merged_model.push_to_hub("ariG23498/g3-od-lora")
+processor.push_to_hub("ariG23498/g3-od-lora")
 
-model.eval()
-generation = model.generate(**sample, max_new_tokens=100)
+merged_model.eval()
+generation = merged_model.generate(**sample, max_new_tokens=100)
 decoded = processor.batch_decode(generation, skip_special_tokens=True)
 
 file_count = 0
 for output_text, sample_image in zip(decoded, sample_images):
     im = sample_image[0]
     width, height = im.size
-    visualize_bounding_boxes(im, output_text, width, height, f"outputs/output_{file_count}.png")
+    visualize_bounding_boxes(im, output_text, width, height, f"{dir_name}/output_{file_count}.png")
     file_count += 1
